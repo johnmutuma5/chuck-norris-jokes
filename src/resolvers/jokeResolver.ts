4520de5f62878bfb3173f414a5b5d756056066b8
John Mutuma <johnmutuma5@gmail.com>
2021-01-24 11:49:12 +0300

    chore: refactor

--- src/resolvers/jokeResolver.ts
+++ src/resolvers/jokeResolver.ts
@@ -2,8 +2,8 @@ import JokesProvider from "src/common/interfaces/jokesProvider";
 import {JokeCategoriesResponse, JokeResponse} from "src/common/types/responses";
 
 export default class JokeResolver {
-
   constructor(private jokesProvider: JokesProvider) { }
+
 
   public getRandomJoke = (categoryName: string): Promise<JokeResponse> => {
     console.assert(this.jokesProvider);
